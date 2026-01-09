e3ddc33ef974fce7ff314ea9283e1a4a43152611
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-01-09 12:38:01 +0100

    initial migration completed

---
+++ FirstAPI/Data/FirstAPIContext.cs
@@ -0,0 +1,16 @@
+﻿using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FirstAPI.Data
+{
+	public class FirstAPIContext : DbContext
+	{
+		public FirstAPIContext(DbContextOptions<FirstAPIContext> options) : base(options) { }
+	
+		public DbSet<Book> Books { get; set; }
+
+
+
+	}
+}
