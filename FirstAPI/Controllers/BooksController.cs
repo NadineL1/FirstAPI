0f5e3f3d09c3d53e8d1b419b8aa08f43613dcbcb
nadine.sorinder@gmail.com <nadine.sorinder@gmail.com>
2026-01-09 19:08:36 +0100

    seeded db, added DI to controller & added async/await.

--- FirstAPI/Controllers/BooksController.cs
+++ FirstAPI/Controllers/BooksController.cs
@@ -1,6 +1,8 @@
-﻿using FirstAPI.Models;
+﻿using FirstAPI.Data;
+using FirstAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstAPI.Controllers
 {
@@ -8,61 +10,25 @@ namespace FirstAPI.Controllers
 	[ApiController]
 	public class BooksController : ControllerBase
 	{
-		static private List<Book> books = new()
-		{
-			new Book
-			{
-				Id = 1,
-				Title = "The Great Gatsby",
-				Author = "F. Scott Fitzgerald",
-				YearPublished = 1925
-			},
-
-			new Book
-			{
-				Id = 2,
-				Title = "To Kill a Mockingbird",
-				Author = "Harper Lee",
-				YearPublished = 1960
-			},
-			new Book
-			{
-				Id = 3,
-				Title = "1984",
-				Author = "George Orwell",
-				YearPublished = 1949
-			},
-
-			new Book
-			{
-				Id = 4,
-				Title = "Pride and Prejudice",
-				Author = "Jane Austen",
-				YearPublished = 1813
-			},
-
-
-			new Book
-			{
-				Id = 5,
-				Title = "Moby Dick",
-				Author = "Herman Melville",
-				YearPublished = 1851
-			}
-		};
-
 		/* Creating HTTP methods */
-		[HttpGet]
-		public ActionResult<List<Book>> GetBooks()
+		/*dependency injection*/
+		private readonly FirstAPIContext _context;
+		public BooksController(FirstAPIContext context)
 		{
-			return Ok(books); //everything worked, api will receive ok 200 status code and the books
+			_context = context;
 		}
 
+		[HttpGet]
+		public async Task<ActionResult<List<Book>>> GetBooks()
+		{
+			return Ok(await _context.Books.ToListAsync()); //everything worked, api will receive ok 200 status code and the books
+		}
+		
 		// requesting specific book
 		[HttpGet("{id}")]
-		public ActionResult<Book> GetBookById(int id)
+		public async Task<ActionResult<Book>> GetBookById(int id)
 		{
-			var book = books.FirstOrDefault(x => x.Id == id);
+			var book = await _context.Books.FindAsync(id);
 			if (book == null)
 				return NotFound();
 
@@ -71,20 +37,22 @@ namespace FirstAPI.Controllers
 
 		// adding new book
 		[HttpPost]
-		public ActionResult<Book> AddBook(Book newBook)
+		public async Task<ActionResult<Book>> AddBook(Book newBook)
 		{
 			if (newBook == null)
 				return BadRequest();
 
-			books.Add(newBook);
+			_context.Books.Add(newBook);
+			await _context.SaveChangesAsync();
+
 			return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
 		}
 
 		// IActionResult because not returning any object, just the status code
 		[HttpPut("{id}")]
-		public IActionResult UpdateBook(int id, Book updatedBook)
+		public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
 		{
-			var book = books.FirstOrDefault(x => x.Id == id);
+			var book = await _context.Books.FindAsync(id);
 			if (book == null)
 				return NotFound();
 
@@ -93,17 +61,21 @@ namespace FirstAPI.Controllers
 			book.Author = updatedBook.Author;
 			book.YearPublished = updatedBook.YearPublished;
 
+			await _context.SaveChangesAsync();
+
 			return NoContent();
 		}
 
 		[HttpDelete("{id}")]
-		public IActionResult DeleteBook(int id)
+		public async  Task<IActionResult> DeleteBook(int id)
 		{
-			var book = books.FirstOrDefault(x => x.Id == id);
+			var book = await _context.Books.FindAsync(id);
 			if (book == null)
 				return NotFound();
 
-			books.Remove(book);
+			_context.Books.Remove(book);
+			await _context.SaveChangesAsync();
+
 			return NoContent();
 		}
 
